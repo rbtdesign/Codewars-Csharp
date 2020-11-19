a6d25e06e922e670d7751ae0f6afb1d211d1674b
codingandcaffeine <codingandcaffeine@outlook.com>
2020-11-19 12:12:55 +0100

    Add project files.

---
+++ AddDistinctStr/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddDistinctStr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "xyaabbbccccdefww";
+            string s2 = "xxxxyyyyabklmopq";
+
+            string concat = s1 + s2;
+
+            List<char> distinctChar = concat.Distinct().ToList();
+
+            distinctChar.Sort();
+
+
+            string output = string.Join("", distinctChar);
+
+            
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+
+            // Solution without passing by a list and sort is to use OrderBy from Linq with Distinct
+            // return string.Concat((s1 + s2).Distinct().OrderBy(x => x));
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "How can mirrors be real if our eyes aren't real";
+
+            string[] strArr = phrase.Split(' ');
+
+            string output = "";
+
+            foreach (string word in strArr)
+            {
+              output = output + char.ToUpper(word[0]) + word.Substring(1) + " ";
+
+            }
+
+            output.Trim();
+
+            
+            
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+
+            // Better way to do it by using TitleCase method
+
+        }
+    }
+}

a4b5217e18087ef72ed11085122494eb3ae4a1b4
codingandcaffeine <codingandcaffeine@outlook.com>
2020-12-04 15:53:30 +0100

    Simple Pig Latin with LINQ only

---
+++ SimplePigLatin/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Pig latin is cool"; //return igPay atinlay siay oolcay
+
+            var output = String.Join(" ",
+                            str.Split(' ')
+                            .Select(word => String.Concat(word.Substring(1), word[0], "ay"))
+                         );
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+        }
+    }
+}

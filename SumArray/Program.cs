2108f10e256d52fc02d882a639b5839e0037d652
codingandcaffeine <codingandcaffeine@outlook.com>
2020-12-13 20:11:01 +0100

    basic linq sum array

---
+++ SumArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] array = { 1, 5.2, 4, 0, -1 };
+
+            // var output = array.Aggregate(0, (double sum, double x)  => sum += x );
+
+            var output = array.Sum();
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+        }
+    }
+}

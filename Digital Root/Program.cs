7fe6e0c95cc57aa93cdf0dd55010b11a8793f07d
codingandcaffeine <codingandcaffeine@outlook.com>
2021-03-04 14:18:32 +0100

    Parity outlier

---
+++ Digital Root/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Root
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //16-- > 1 + 6 = 7
+            //942-- > 9 + 4 + 2 = 15-- > 1 + 5 = 6
+            //132189-- > 1 + 3 + 2 + 1 + 8 + 9 = 24-- > 2 + 4 = 6
+            //493193-- > 4 + 9 + 3 + 1 + 9 + 3 = 29-- > 2 + 9 = 11
+
+            int num = 493193;
+
+            //int res = (int)(1 + (num - 1) % 9);
+
+            int res = (int)(num % 9);
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+
+            //int[] res = getIntArr(num);
+
+            //int tot = res.Sum(); 
+
+            //while(tot >= 10)
+            //{
+            //    res = getIntArr(tot);
+            //    tot = res.Sum();
+            //}
+
+
+            //return tot;
+
+
+        }
+
+        //static int[] getIntArr(long num)
+        //{
+        //    List<int> listDigits = new List<int>();
+        //    while (num > 0)
+        //    {
+        //        listDigits.Add((int)(num % 10));
+        //        num = num / 10;
+        //    }
+        //    listDigits.Reverse();
+        //    return listDigits.ToArray();
+        //}
+    }
+}
---
+++ Parity outlier/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parity_outlier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = new int[] {2, 4, 0, 100, 4, 11, 2602, 36};
+
+            var res = integers.GroupBy(x => x % 2).Where(groupe => groupe.Count() == 1).FirstOrDefault().First();
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+        
+        }
+    }
+}
